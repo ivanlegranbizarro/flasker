626cba975b6a61cc62f11c363d245b1f3e11fbdc
Iván Legrán <68439095+ivanlegranbizarro@users.noreply.github.com>
2022-02-08 09:46:02 +0100

    Login User

--- app.py
+++ app.py
@@ -12,6 +12,8 @@ from datetime import datetime
 
 from werkzeug.security import generate_password_hash, check_password_hash
 
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+
 # Create a Flask Instance
 
 app = Flask(__name__)
@@ -24,6 +26,16 @@ app.config['SECRET_KEY'] = 'mysecretkey'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+# Flask Login Stuff
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return Users.query.get(int(user_id))
+
 
 # Return some JSON
 @app.route('/date')
@@ -33,7 +45,7 @@ def get_current_date():
 
 
 # Create a Model
-class Users(db.Model):
+class Users(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(50), nullable=False, unique=True)
@@ -262,3 +274,27 @@ def delete_user(id):
     except:
         flash('Error deleting user!', 'danger')
         return redirect(url_for('add_user'))
+
+
+# Login Form
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password_hash = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
+
+# Create Login Page
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = Users.query.filter_by(email=form.email.data).first()
+        # Check the hash
+        if check_password_hash(user.password_hash, form.password_hash.data):
+            login_user(user)
+            flash('You have successfully logged in!', 'success')
+            return redirect(url_for('index'))
+        else:
+            flash('Login unsuccessful. Please check email and password',
+                  'danger')
+    return render_template('login.html', form=form)
